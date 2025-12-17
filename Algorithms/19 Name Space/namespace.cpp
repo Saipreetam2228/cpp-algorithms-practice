d334e8e3a66bf5f1ae91996ba9f4f97de24bd58f
Saipreetam <saipreetam.official@gmail.com>
2025-12-17 10:52:25 +0530

    Add files via upload

---
+++ Algorithms/19 Name Space/namespace.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+namespace vinod
+{
+	int val=500;
+	void display()
+	{
+		cout<<"welome namespace\n";
+	}
+  class student
+	{
+	  public:
+		void set()	
+	}	  
+}
+
+
+
+int main()
+{
+
+	return 0;
+}
